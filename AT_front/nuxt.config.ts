d53e880a507da8b12f92573c7b6509fbb00f8ca1
ryu <wocks1123@gmail.com>
2023-03-16 15:50:15 +0900

    모듈 및 기본 설정 추가

--- AT_front/nuxt.config.ts
+++ AT_front/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        '@nuxtjs/tailwindcss'
-    ]
+        '@nuxtjs/tailwindcss',
+        'nuxt-icon',
+        '@pinia/nuxt',
+        '@vueuse/nuxt',
+    ],
+    plugins: [
+        { src: '@/plugins/detector.js', mode: 'client' }
+    ],
 })
